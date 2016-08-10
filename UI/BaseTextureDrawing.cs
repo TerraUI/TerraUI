f614bbc720c3e198d0c89473e74068e83d2a30c4
Alana Gilston <alana@abluescarab.us>
2016-08-10 05:21:59 -0700

    Removed mod reference

--- UI/BaseTextureDrawing.cs
+++ UI/BaseTextureDrawing.cs
@@ -16,22 +16,21 @@ namespace TerraUI {
         }
 
         public static void DrawTerrariaStyledBox(SpriteBatch sb, Color colour, Rectangle rect, bool solid = false) {
-            Mod mod = UIUtils.Mod;
             string add = "";
 
             if(solid) {
                 add = "Solid";
             }
 
-            sb.Draw(mod.GetTexture("Textures/Corner" + add), new Vector2(rect.X, rect.Y), colour);
-            sb.Draw(mod.GetTexture("Textures/Corner" + add), new Vector2(rect.X + rect.Width, rect.Y), null, colour, (float)(Math.PI / 2), default(Vector2), 1f, SpriteEffects.None, 0f);
-            sb.Draw(mod.GetTexture("Textures/Corner" + add), new Vector2(rect.X + rect.Width, rect.Y + rect.Height), null, colour, (float)(Math.PI), default(Vector2), 1f, SpriteEffects.None, 0f);
-            sb.Draw(mod.GetTexture("Textures/Corner" + add), new Vector2(rect.X, rect.Y + rect.Height), null, colour, (float)(Math.PI * 1.5), default(Vector2), 1f, SpriteEffects.None, 0f);
-            sb.Draw(mod.GetTexture("Textures/Side" + add), new Rectangle(rect.X + 16, rect.Y, rect.Width - 32, 16), colour);
-            sb.Draw(mod.GetTexture("Textures/Side" + add), new Rectangle(rect.X + rect.Width, rect.Y + 16, rect.Height - 32, 16), null, colour, (float)(Math.PI / 2), default(Vector2), SpriteEffects.None, 0f);
-            sb.Draw(mod.GetTexture("Textures/Side" + add), new Rectangle(rect.X + rect.Width - 16, rect.Y + rect.Height, rect.Width - 32, 16), null, colour, (float)(Math.PI), default(Vector2), SpriteEffects.None, 0f);
-            sb.Draw(mod.GetTexture("Textures/Side" + add), new Rectangle(rect.X, rect.Y + rect.Height - 16, rect.Height - 32, 16), null, colour, (float)(Math.PI * 1.5), default(Vector2), SpriteEffects.None, 0f);
-            sb.Draw(mod.GetTexture("Textures/Background" + add), new Rectangle(rect.X + 16, rect.Y + 16, rect.Width - 32, rect.Height - 32), null, colour);
+            sb.Draw(UIUtils.Mod.GetTexture("Textures/Corner" + add), new Vector2(rect.X, rect.Y), colour);
+            sb.Draw(UIUtils.Mod.GetTexture("Textures/Corner" + add), new Vector2(rect.X + rect.Width, rect.Y), null, colour, (float)(Math.PI / 2), default(Vector2), 1f, SpriteEffects.None, 0f);
+            sb.Draw(UIUtils.Mod.GetTexture("Textures/Corner" + add), new Vector2(rect.X + rect.Width, rect.Y + rect.Height), null, colour, (float)(Math.PI), default(Vector2), 1f, SpriteEffects.None, 0f);
+            sb.Draw(UIUtils.Mod.GetTexture("Textures/Corner" + add), new Vector2(rect.X, rect.Y + rect.Height), null, colour, (float)(Math.PI * 1.5), default(Vector2), 1f, SpriteEffects.None, 0f);
+            sb.Draw(UIUtils.Mod.GetTexture("Textures/Side" + add), new Rectangle(rect.X + 16, rect.Y, rect.Width - 32, 16), colour);
+            sb.Draw(UIUtils.Mod.GetTexture("Textures/Side" + add), new Rectangle(rect.X + rect.Width, rect.Y + 16, rect.Height - 32, 16), null, colour, (float)(Math.PI / 2), default(Vector2), SpriteEffects.None, 0f);
+            sb.Draw(UIUtils.Mod.GetTexture("Textures/Side" + add), new Rectangle(rect.X + rect.Width - 16, rect.Y + rect.Height, rect.Width - 32, 16), null, colour, (float)(Math.PI), default(Vector2), SpriteEffects.None, 0f);
+            sb.Draw(UIUtils.Mod.GetTexture("Textures/Side" + add), new Rectangle(rect.X, rect.Y + rect.Height - 16, rect.Height - 32, 16), null, colour, (float)(Math.PI * 1.5), default(Vector2), SpriteEffects.None, 0f);
+            sb.Draw(UIUtils.Mod.GetTexture("Textures/Background" + add), new Rectangle(rect.X + 16, rect.Y + 16, rect.Width - 32, rect.Height - 32), null, colour);
         }
     }
 }
