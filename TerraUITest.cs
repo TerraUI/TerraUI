c2e073348d604387c6c0d94de77103c60df84829
Alana Gilston <alana@abluescarab.us>
2016-08-18 04:02:44 -0700

    Added UIProgressBar example

--- TerraUITest.cs
+++ TerraUITest.cs
@@ -4,20 +4,13 @@
  * todo: update description.txt
  */
 
-using System;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
 using TerraUI;
-using System.Diagnostics;
 
 namespace TerraUITest {
     public class TerraUITest : Mod {
-        UITextBox tb;
-        UIButton btn;
-        //UIScrollbar scrl;
-
         public override void Load() {
             Properties = new ModProperties() {
                 Autoload = true,
@@ -26,51 +19,14 @@ namespace TerraUITest {
                 AutoloadSounds = true
             };
 
-            tb = new UITextBox(new Vector2(500, 500), new Vector2(100, 22), Main.fontItemStack, "Test text");
-            btn = new UIButton(new Vector2(500, 550), new Vector2(100, 22), DoClick, Main.fontItemStack, "Click Here");
-            //scrl = new UIScrollbar(new Vector2(500, 600), new Vector2(100, 22), Orientations.Horizontal);
-            btn.ClickBackColor = Color.Red;
-            btn.ClickBorderColor = Color.Blue;
-            btn.ClickTextColor = Color.Green;
             UIUtils.Mod = this;
             UIUtils.Subdirectory = "TerraUI";
         }
 
-        private void DoClick() {
-            Main.NewText("Clicked!");
-        }
-
         public override void PostDrawInterface(SpriteBatch spriteBatch) {
-            if(tb != null) {
-                tb.Draw(spriteBatch);
-            }
-
-            if(btn != null) {
-                btn.Draw(spriteBatch);
-            }
-
-            //if(scrl != null) {
-            //    scrl.Draw(spriteBatch);
-            //}
-
+            TerraUITestPlayer player = Main.player[Main.myPlayer].GetModPlayer<TerraUITestPlayer>(this);
+            player.DrawUI(spriteBatch);
             base.PostDrawInterface(spriteBatch);
-        }
-
-        public override void PostUpdateInput() {
-            if(tb != null) {
-                tb.Update();
-            }
-
-            if(btn != null) {
-                btn.Update();
-            }
-
-            //if(scrl != null) {
-            //    scrl.Update();
-            //}
-
-            UIUtils.UpdateInput();
-            base.PostUpdateInput();
         }
     }
 }
---
+++ TerraUITestPlayer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+using TerraUI;
+
+namespace TerraUITest {
+    public class TerraUITestPlayer : ModPlayer {
+        UITextBox tb;
+        UIButton btn;
+        UIProgressBar bar;
+        //UIScrollbar scrl;
+
+        public override bool Autoload(ref string name) {
+            return true;
+        }
+
+        public override void Initialize() {
+            tb = new UITextBox(new Vector2(500, 500), new Vector2(100, 22), Main.fontItemStack, "Test text");
+            btn = new UIButton(new Vector2(500, 550), new Vector2(100, 22), DoClick, Main.fontItemStack, "Click Here");
+            bar = new UIProgressBar(new Vector2(500, 600), new Vector2(100, 22));
+            //scrl = new UIScrollbar(new Vector2(500, 600), new Vector2(100, 22), Orientations.Horizontal);
+
+            btn.ClickBackColor = Color.Red;
+            btn.ClickBorderColor = Color.Blue;
+            btn.ClickTextColor = Color.Green;
+
+            bar.Maximum = 100;
+            bar.BarMargin = new Vector2(0, 5);
+
+            base.Initialize();
+        }
+
+        private void DoClick() {
+            Main.NewText("Clicked!");
+        }
+
+        public override void PreUpdate() {
+            UpdateUI();
+            base.PreUpdate();
+        }
+
+        public void DrawUI(SpriteBatch spriteBatch) {
+            if(tb != null) {
+                tb.Draw(spriteBatch);
+            }
+
+            if(btn != null) {
+                btn.Draw(spriteBatch);
+            }
+
+            if(bar != null) {
+                bar.Draw(spriteBatch);
+            }
+
+            //if(scrl != null) {
+            //    scrl.Draw(spriteBatch);
+            //}
+        }
+
+        public void UpdateUI() {
+            if(tb != null) {
+                tb.Update();
+            }
+
+            if(btn != null) {
+                btn.Update();
+            }
+
+            if(bar != null) {
+                bar.Step();
+
+                if(bar.Value == bar.Maximum) {
+                    bar.Reset();
+                }
+            }
+
+            //if(scrl != null) {
+            //    scrl.Update();
+            //}
+
+            UIUtils.UpdateInput();
+        }
+    }
+}
