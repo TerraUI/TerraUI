de121c8a723cab830a8a751a030a4f56c0e7eb57
Alana Gilston <alana@abluescarab.us>
2016-08-18 03:58:32 -0700

    Added UIProgressBar

--- Utils/UIColors.cs
+++ Utils/UIColors.cs
@@ -26,6 +26,13 @@ namespace TerraUI {
             public static readonly Color BorderColor = Color.Black;
         }
 
+        public static class ProgressBar {
+            public static readonly Color BackColor = UIColors.BackColor;
+            public static readonly Color BarColor = Color.White;
+            public static readonly Color FinishedBarColor = Color.White;
+            public static readonly Color BorderColor = Color.White;
+        }
+
         public static readonly Color LightBackColor = new Color(100, 102, 190);
         public static readonly Color LightBackColorTransparent = new Color(100, 102, 190) * 0.7f;
         public static readonly Color DarkBackColor = new Color(63, 65, 151);
