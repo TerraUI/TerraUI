ef64a5fa21c38c3a6479868603bfc379e7e8ce13
Alana Gilston <alana@abluescarab.us>
2016-08-18 20:07:07 -0700

    Added ValueChanged and ValueChangedEventsArgs

--- Utils/Events.cs
+++ Utils/Events.cs
@@ -8,6 +8,17 @@ namespace TerraUI {
     public delegate void StepHandler(UIObject sender);
     public delegate void DrawHandler(UIObject sender, SpriteBatch spriteBatch);
     public delegate bool ConditionHandler(Item item);
+    public delegate bool ValueChanged<T>(UIObject sender, ValueChangedEventArgs<T> e);
+
+    public class ValueChangedEventArgs<T> {
+        public T PreviousValue { get; private set; }
+        public T Value { get; private set; }
+
+        public ValueChangedEventArgs(T previousValue, T value) {
+            PreviousValue = previousValue;
+            Value = value;
+        }
+    }
 
     public class ClickEventArgs {
         public Vector2 Position { get; private set; }
