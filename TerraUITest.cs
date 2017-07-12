863b8b85ac0fc4a504e7a6df7a9df3de9c786921
Alana Gilston <alana@abluescarab.us>
2017-07-12 11:28:26 -0700

    Initial rewrite commit

---
+++ TerraUITest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace TerraUITest {
+    class TerraUITest : Mod {
+        public TerraUITest() {
+            Properties = new ModProperties() {
+                Autoload = true,
+                AutoloadGores = true,
+                AutoloadSounds = true
+            };
+        }
+    }
+}
