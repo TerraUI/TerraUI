fc86ff16276687b149f6028213e8ab05e896d0df
Alana Gilston <alana@abluescarab.us>
2017-07-18 11:48:20 -0700

    Added mod files

---
+++ TerraUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace TerraUI {
+    class TerraUI : Mod {
+        public TerraUI() {
+            Properties = new ModProperties() {
+                Autoload = true,
+                AutoloadGores = true,
+                AutoloadSounds = true
+            };
+        }
+    }
+}
